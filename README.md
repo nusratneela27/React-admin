ab512cfb4ba5bfb68c6537f80f4bf0ebb0a97144
neela <nusratneela27@gmail.com>
2023-10-23 17:16:17 +0600

    login page

---
+++ README.md
@@ -0,0 +1,2 @@
+# Live link
+- [react-admins.netlify.app]
---
+++ src/post.jsx
@@ -0,0 +1,43 @@
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, ReferenceInput, TextInput, Create, useRecordContext } from "react-admin";
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" />,
+];
+
+export const PostList = () => (
+    <List filters={postFilters}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users" link="show" />
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+const PostTitle = () => {
+    const record = useRecordContext();
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = () => (
+    <Edit title={<PostTitle />}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Create>
+);
